3b871df11e7f811ad0ad9b1aacca562586e3b960
zgma-test <uditharosha+zgma@gmail.com>
2019-08-01 15:52:36 +0530

    wwd

---
+++ dwd/lambda.js
@@ -0,0 +1,20 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = function (event, context, callback) {
+
+    sns.publish({
+        Message: 'hello',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+    callback(null, { "message": "Successfully executed" });
+}
