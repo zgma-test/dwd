4ac511d1c33ae4f4e66191346871a6291ce8bf4e
zgma-test <uditharosha+zgma@gmail.com>
2019-08-01 17:01:22 +0530

    wd

--- dwd/lambda.js
+++ dwd/lambda.js
@@ -1,4 +1,5 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
@@ -15,6 +16,37 @@ exports.handler = function (event, context, callback) {
         .catch(err => {
             // error handling goes here
         });
+
+    s3.listObjects({
+        'Bucket': 'bucket-001',
+        'MaxKeys': 10,
+        'Prefix': ''
+    }).promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                Contents: [
+                    {
+                       ETag: "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
+                       Key: "example1.jpg",
+                       LastModified: "<Date Representation>",
+                       Owner: {
+                          DisplayName: "myname",
+                          ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
+                       },
+                       Size: 11,
+                       StorageClass: "STANDARD"
+                    },
+                    // {...}
+                ]
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+
 
     callback(null, { "message": "Successfully executed" });
 }
