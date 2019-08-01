c266454babc5d16c6308e1ce4a0def971dfa4dc4
zgma-test <uditharosha+zgma@gmail.com>
2019-08-01 17:03:51 +0530

    dwd

--- dwd/lambda.js
+++ dwd/lambda.js
@@ -17,35 +17,6 @@ exports.handler = function (event, context, callback) {
             // error handling goes here
         });
 
-    s3.listObjects({
-        'Bucket': 'bucket-001',
-        'MaxKeys': 10,
-        'Prefix': ''
-    }).promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                Contents: [
-                    {
-                       ETag: "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
-                       Key: "example1.jpg",
-                       LastModified: "<Date Representation>",
-                       Owner: {
-                          DisplayName: "myname",
-                          ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
-                       },
-                       Size: 11,
-                       StorageClass: "STANDARD"
-                    },
-                    // {...}
-                ]
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
 
 
     callback(null, { "message": "Successfully executed" });
