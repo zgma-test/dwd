33f7c85bdff3f3614fe45f6b47851a1a29b4a906
zgma-test <uditharosha+zgma@gmail.com>
2019-08-27 10:31:16 +0530

    f

--- dwd/lambda.js
+++ dwd/lambda.js
@@ -1,23 +1,29 @@
 let AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-const sns = new AWS.SNS();
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
-
-    sns.publish({
-        Message: 'hello',
-        MessageAttributes: {},
-        MessageStructure: 'String',
-        TopicArn: 'arn:aws:sns:us-east-1:318300609668:dynamodb'
+    sqs.receiveMessage({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/MyQueue`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
     }).promise()
-        .then(data => {
-            // your code goes here
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                });
+            } else {
+                // No messages to process
+            }
         })
         .catch(err => {
             // error handling goes here
         });
-
-
 
     callback(null, { "message": "Successfully executed" });
 }
